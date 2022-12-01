de09c765280b31d9029ba7f63e5c41dac9085ebc
Stefan Karlsson <stefan@worldfavor.com>
2022-12-01 09:55:41 +0100

    modify newDay to be able to add days in any given year

--- cs/newDay.sh
+++ cs/newDay.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 determineNextDay () {
-  array=($(ls ./solutions/2015))
+  array=($(ls ./solutions/"$YEAR"))
   highestNumber=1;
   
   for i in "${array[@]}"
@@ -11,20 +11,28 @@ determineNextDay () {
         highestNumber=$dayNumber   
      fi
   done  
-  echo $((highestNumber + 1))
+  [[ $highestNumber = 1 ]] && output=1 || output=$((highestNumber + 1))
+  echo $output
 }
 
+if test -z "$1"
+then
+  echo "No year was provided. Example usage: $(tput setaf 3) sh newDay 2022"
+  exit 1
+fi
+
+YEAR="$1"
 DAY_NAME=Day"$(determineNextDay)"
 TEST_FILE_NAME="$DAY_NAME"Tests
 
-mkdir solutions/2015/"$DAY_NAME"
-cd solutions/2015/"$DAY_NAME" || exit 1
+mkdir solutions/"$YEAR"/"$DAY_NAME"
+cd solutions/"$YEAR"/"$DAY_NAME" || exit 1
 
 touch input.txt
 touch "$DAY_NAME".cs
 
 cat > "$DAY_NAME".cs << EOF
-namespace solutions._2015;
+namespace solutions._$YEAR;
 
 public static class $DAY_NAME
 {
@@ -42,23 +50,23 @@ public static class $DAY_NAME
 
     private static string GetInput()
     {
-        return File.ReadAllText("./2015/$DAY_NAME/input.txt");
+        return File.ReadAllText("./$YEAR/$DAY_NAME/input.txt");
     }
 }
 EOF
 
 cd ../../..
 
-cd solutions-tests/2015 || exit 1
+cd solutions-tests/"$YEAR" || exit 1
 
 touch "$DAY_NAME"Tests.cs
 
 cat > "$DAY_NAME"Tests.cs << EOF
 using FluentAssertions;
 using NUnit.Framework;
-using solutions._2015;
+using solutions._$YEAR;
 
-namespace solutions_tests._2015;
+namespace solutions_tests._$YEAR;
 
 public class $TEST_FILE_NAME
 {
