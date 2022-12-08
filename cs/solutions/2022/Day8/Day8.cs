fbd095b2f6d2c597d19080eb9bef7aeb71851b64
Stefan Karlsson <stefan@worldfavor.com>
2022-12-08 13:43:40 +0100

    recfactored day8 into single method

--- cs/solutions/2022/Day8/Day8.cs
+++ cs/solutions/2022/Day8/Day8.cs
@@ -2,138 +2,43 @@ namespace solutions._2022;
 
 public static class Day8
 {
+    private class Tree
+    {
+        public bool Visible { get; set; } = false;
+        public int ScenicScore { get; set; }
+        public required int Height { get; init; }
+    }
+    
     public static int Part_1(string? input = null)
     {
         input ??=  GetInput();
         var grid =  input.Split('\n').Select(row => row.ToCharArray().Select(c => int.Parse(c.ToString())));
 
-        return FindVisibleTrees(grid).Count();
+        return GetTrees(grid).Count(t => t.Visible);
     }
 
-    private static IEnumerable<int> FindVisibleTrees(IEnumerable<IEnumerable<int>> grid)
+    private static IEnumerable<Tree> GetTrees(IEnumerable<IEnumerable<int>> grid)
     {
-        var enumerable = grid as IEnumerable<int>[] ?? grid.ToArray();
-        var visibleTreeHeights = new List<int>();
-
-        for (var rowIndex = 0; rowIndex < enumerable.Length; rowIndex++)
-        {
-            var currentRow = enumerable[rowIndex].ToArray();
-            for (var columnIndex = 0; columnIndex < enumerable[rowIndex].Count(); columnIndex++)
-            {
-                var currentHeight = currentRow[columnIndex];
-                var visible = columnIndex == 0 || columnIndex == currentRow.Length-1 || rowIndex == 0 || rowIndex == enumerable.Length-1;
-                
-                if (visible)
-                {
-                    visibleTreeHeights.Add(currentHeight);
-                    continue;
-                }
-                
-                // go left
-                for (var k = columnIndex - 1; k >= 0; k--)
-                {
-                    if (currentRow[k] >= currentHeight)
-                    {
-                        visible = false;
-                        break;
-                    }
-                
-                    visible = true;
-                }
-                
-                if (visible)
-                {
-                    visibleTreeHeights.Add(currentHeight);
-                    continue;
-                }
-                
-                // go up
-                for (var k = rowIndex - 1; k >= 0; k--)
-                {
-                    if (enumerable[k].ToArray()[columnIndex] >= currentHeight)
-                    {
-                        visible = false;
-                        break;
-                    }
-                
-                    visible = true;
-                }
-
-                if (visible)
-                {
-                    visibleTreeHeights.Add(currentHeight);
-                    continue;
-                }
-                
-                // go right
-                for (var k = columnIndex + 1; k < currentRow.Length; k++)
-                {
-                    if (currentRow[k] >= currentHeight)
-                    {
-                        visible = false;
-                        break;
-                    }
-                
-                    visible = true;
-                }
-                
-                if (visible)
-                {
-                    visibleTreeHeights.Add(currentHeight);
-                    continue;
-                }
-                
-                // go down
-                for (var k = rowIndex + 1; k < enumerable.Length; k++)
-                {
-                    if (enumerable[k].ToArray()[columnIndex] >= currentHeight)
-                    {
-                        visible = false;
-                        break;
-                    }
-                
-                    visible = true;
-                }
-                
-                if (visible)
-                {
-                    visibleTreeHeights.Add(currentHeight);
-                }
-            }
-        }
+        var trees =  new List<Tree>();
         
-        return visibleTreeHeights;
-    }
-
-    public static int Part_2(string? input = null)
-    {
-        input ??= GetInput();
-        var grid =  input.Split('\n').Select(row => row.ToCharArray().Select(c => int.Parse(c.ToString())));
-
-        return GetScenicScores(grid).Max();
-    }
-
-    private static IEnumerable<int> GetScenicScores(IEnumerable<IEnumerable<int>> grid)
-    {
         var enumerable = grid as IEnumerable<int>[] ?? grid.ToArray();
-        var scenicScores = new List<int>();
 
         for (var rowIndex = 0; rowIndex < enumerable.Length; rowIndex++)
         {
             var currentRow = enumerable[rowIndex].ToArray();
             for (var columnIndex = 0; columnIndex < enumerable[rowIndex].Count(); columnIndex++)
             {
-                var currentHeight = currentRow[columnIndex];
+                var currentTree = new Tree{ Height = currentRow[columnIndex]};
                 var outerTree = columnIndex == 0 || columnIndex == currentRow.Length-1 || rowIndex == 0 || rowIndex == enumerable.Length-1;
 
-                var left = 0;
-                var top = 0;
-                var right = 0;
-                var down = 0;
+                int left = 0, top = 0, right = 0, down = 0;
+                bool visibleLeft = true, visibleTop = true, visibleRight = true, visibleDown = true;
                 
                 if (outerTree)
                 {
-                    scenicScores.Add(0); // all our outer stuff is going to be 0
+                    currentTree.Visible = true;
+                    currentTree.ScenicScore = 0;
+                    trees.Add(currentTree);
                     continue;
                 }
                 
@@ -141,7 +46,8 @@ public static class Day8
                 for (var k = 1; k <= columnIndex; k++)
                 {
                     left = k;
-                    if (currentRow[columnIndex - k] < currentHeight) continue;
+                    if (currentRow[columnIndex - k] < currentTree.Height) continue;
+                    visibleLeft = false;
                     break;
                 }
 
@@ -149,7 +55,8 @@ public static class Day8
                 for (var k = 1; k <= rowIndex; k++)
                 {
                     top = k;
-                    if (enumerable[rowIndex-k].ToArray()[columnIndex] < currentHeight) continue;
+                    if (enumerable[rowIndex-k].ToArray()[columnIndex] < currentTree.Height) continue;
+                    visibleTop = false;
                     break;
                 }
 
@@ -157,7 +64,8 @@ public static class Day8
                 for (var k = 1; k < currentRow.Length - columnIndex; k++)
                 {
                     right = k;
-                    if (currentRow[columnIndex + k] < currentHeight) continue;
+                    if (currentRow[columnIndex + k] < currentTree.Height) continue;
+                    visibleRight = false;
                     break;
                 }
 
@@ -165,19 +73,30 @@ public static class Day8
                 for (var k = 1; k < enumerable.Length - rowIndex; k++)
                 {
                     down = k;
-                    if (enumerable[rowIndex + k].ToArray()[columnIndex] < currentHeight) continue;
+                    if (enumerable[rowIndex + k].ToArray()[columnIndex] < currentTree.Height) continue;
+                    visibleDown = false;
                     break;
                 }
 
-                var scenicScore = left * top * right * down;
+                currentTree.Visible = visibleLeft || visibleTop || visibleRight || visibleDown;
                 
-                scenicScores.Add(scenicScore);
+                currentTree.ScenicScore = left * top * right * down;
+                
+                trees.Add(currentTree);
             }
         }
 
-        return scenicScores;
+        return trees;
     }
-    
+
+    public static int Part_2(string? input = null)
+    {
+        input ??= GetInput();
+        var grid =  input.Split('\n').Select(row => row.ToCharArray().Select(c => int.Parse(c.ToString())));
+
+        return GetTrees(grid).Max(t => t.ScenicScore);
+    }
+
     private static string GetInput()
     {
         return File.ReadAllText("./2022/Day8/input.txt");
