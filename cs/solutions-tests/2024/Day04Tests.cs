a8742f98b256abb16484a5a308fae87d9f9fac3b
Danneil Stefan <stefan.danneil@lakemedelsverket.se>
2024-12-13 11:34:26 +0100

    day 4 part 1 solved

--- cs/solutions-tests/2024/Day04Tests.cs
+++ cs/solutions-tests/2024/Day04Tests.cs
@@ -6,7 +6,19 @@ namespace solutions_tests._2024;
 
 public class Day04Tests
 {
-    [TestCase("", 0)]
+    [TestCase(
+        """
+        MMMSXXMASM
+        MSAMXMSMSA
+        AMXSXMAAMM
+        MSAMASMSMX
+        XMASAMXAMM
+        XXAMMXXAMA
+        SMSMSASXSS
+        SAXAMASAAA
+        MAMMMXMMMM
+        MXMXAXMASX
+        """, 18)]
     public void it_solves_part_1_according_to_examples(string input, int expected)
     {
         Day04.Part_1(input).Should().Be(expected);
--- cs/solutions/2024/Day04.cs
+++ cs/solutions/2024/Day04.cs
@@ -5,9 +5,101 @@ public class Day04 : IDay
     public static int Part_1(string? input = null)
     {
         input ??=  GetInput();
-        throw new NotImplementedException();
+        var rows = input.Split('\n');
+        const string targetString = "XMAS";
+        const string reversedTargetString = "SAMX";
+        var output = 0;
+
+        for (var i = 0; i < rows.Length; i++)
+        {
+            for (var j = 0; j < rows[i].Length; j++)
+            {
+                var currentLetter = rows[i][j];
+
+                if (currentLetter == targetString.First())
+                {
+                    if (rows[i][j..].StartsWith(targetString))
+                        output++;
+                    
+                    var verticalString = BuildVerticalStringFromIndex(rows, i, j);
+                    if (verticalString == targetString)
+                        output++;
+                    
+                    var downWardRightDiagonalString = BuildDownRightDiagonalFromIndex(rows, i, j);
+                    if (downWardRightDiagonalString == targetString)
+                        output++;
+                    
+                    var downWardLeftDiagonalString = BuildDownLeftDiagonalFromIndex(rows, i, j);
+                    if (downWardLeftDiagonalString == targetString)
+                        output++;
+                }
+                
+                if (currentLetter == targetString.Last())
+                {
+                    if (rows[i][j..].StartsWith(reversedTargetString))
+                        output++;
+                    
+                    var verticalString = BuildVerticalStringFromIndex(rows, i, j);
+                    if (verticalString == reversedTargetString)
+                        output++;
+                    
+                    var downWardDiagonalString = BuildDownRightDiagonalFromIndex(rows, i, j);
+                    if (downWardDiagonalString == reversedTargetString)
+                        output++;
+                    
+                    var downWardLeftDiagonalString = BuildDownLeftDiagonalFromIndex(rows, i, j);
+                    if (downWardLeftDiagonalString == reversedTargetString)
+                        output++;
+                }
+            }
+        }
+        
+        return output;
     }
 
+    private static string BuildVerticalStringFromIndex(string[] rows, int rowIndex, int columnIndex)
+    {
+        var output = "";
+        
+        for (var i = rowIndex; i < rows.Length && output.Length < 4; i++)
+        {
+            output += rows[i][columnIndex];
+        };
+        
+        return output;
+    }
+    
+    private static string BuildDownRightDiagonalFromIndex(string[] rows, int rowIndex, int columnIndex)
+    {
+        var output = "";
+        
+        for (var i = rowIndex; i < rows.Length && output.Length < 4; i++)
+        {
+            if (columnIndex + output.Length >= rows[i].Length)
+                break;
+            
+            output += rows[i][columnIndex + output.Length];
+        };
+        
+        return output;
+    }
+    
+    private static string BuildDownLeftDiagonalFromIndex(string[] rows, int rowIndex, int columnIndex)
+    {
+        var output = "";
+        
+        for (var i = rowIndex; i < rows.Length && output.Length < 4; i++)
+        {
+            if (columnIndex - output.Length < 0)
+                break;
+            
+            output += rows[i][columnIndex-output.Length];
+        };
+        
+        return output;
+    }
+    
+    
     public static int Part_2(string? input = null)
     {
         input ??= GetInput();
